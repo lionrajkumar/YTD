b148fb86f661f58a115cca7958eb0e85e178acac
Rajkumar <rajkumar@integrass.com>
2023-01-26 02:22:48 +0530

    Youtube video and audio download functionality complated

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # YTD
 YTD - Download videos and extract audios from YouTube.
+
+### Note
+The download speeds are too slow! I am getting less than 100KBps in a 100Mbps connection! Without faster downloads there is no meaning in using this.
+
+### Update
+accept a youtube short link like "https://youtu.be/5cpIZ8zHHXw", should update your downloaded index.php file
+
+### Reference
+1. [Khokon M](https://www.blogdesire.com/author/khokon/)
+2. [How to create a YouTube video downloader using PHP?](https://www.blogdesire.com/how-to-create-a-youtube-video-downloader-using-php/) 
---
+++ downloader.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by lionrajkumar.
+ * User: Rajkumar
+ * Date: 26-01-2023
+ * Time: 12:01 AM
+ */
+
+$downloadURL = urldecode($_GET['link']);
+$type = urldecode($_GET['type']);
+$title = urldecode($_GET['title']);
+$fileName = $title.'.'.$type;
+
+
+if (!empty($downloadURL) && substr($downloadURL, 0, 8) === 'https://') {
+    header("Cache-Control: public");
+    header("Content-Description: File Transfer");
+    header("Content-Disposition: attachment;filename=\"$fileName\"");
+    header("Content-Transfer-Encoding: binary");
+
+    readfile($downloadURL);
+
+}
